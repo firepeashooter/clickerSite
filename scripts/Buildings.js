f5b1e356338c47a7e3869469538aee4af624e39b
Ben Braun <benjamin.braun2003@gmail.com>
2025-04-21 15:31:37 -0400

    Multiple Building Functionality
    Refactored code to support multiple building upgrades. Added a global CPS variable to keep track of how many cookies all buildings should be making and fixed how the interval was called accordingly.

--- scripts/Buildings.js
+++ scripts/Buildings.js
@@ -6,24 +6,35 @@ class Building{
         this.cost = cost
     }
 
-    produceCookies(){
-        cookies += (this.cps * this.level)
-        document.getElementById('counter').innerHTML = cookies;
-    }
-
 }
+
+//Global cps
+let globalCPS = 0;
 
 //Building Names and Id of the element that displays it's information
 const brettFactory = new Building('Brett Factory', 0, 1, 10);
 const factoryID = "brettFactoryLevel";
-const factoryCostID = "brettFactoryCost"
+const factoryCostID = "brettFactoryCost";
+
+
+const brettFarm = new Building('Brett Farm', 0, 5, 100);
+const farmID = "brettFarmLevel";
+const farmCostID = "brettFarmCost";
 
 
 let intervalID;
 
 
+function produceCookies(){
+    cookies += globalCPS;
+    document.getElementById('counter').innerHTML = cookies;
+    console.log(globalCPS)
+}
+
+
 
 function buyBuilding(building, id, costID){
+
 
     if (cookies >= building.cost){ //Buy the Building Successfully
         
@@ -34,15 +45,16 @@ function buyBuilding(building, id, costID){
         document.getElementById(id).innerHTML = building.level;
         document.getElementById(costID).innerHTML = building.cost;
 
+        //Updates the global cps counter
+        globalCPS += building.cps 
+
         //Reset the interval so it updates the count and so that there is only 1 interval happening at a time
         if (intervalID){
             clearInterval(intervalID)
         }
 
-        //Every second we call the produce cookies function on building
-        intervalID = setInterval(() => {
-            building.produceCookies(); // Arrow function magic 
-        }, 1000);
+        //Every second we call produceCookies function which updates the cookie with the global cps
+        intervalID = setInterval(produceCookies, 100);
 
     } else{ //Not enough cookies
         console.log("You do not have enough cookies to make this purchase")
@@ -56,7 +68,8 @@ function sellBuilding(building, id, costID){
     //If building is level 0 or lower don't update the level again and end the interval (timer)
     if (building.level <= 0){
         console.log(building.name +  ' already all sold')
-        clearInterval(intervalID)
+        
+
     }else{ //Sucessfully sell the building
 
         //Update building level and cost and cast it to the website
@@ -65,6 +78,10 @@ function sellBuilding(building, id, costID){
         cookies += building.cost * 0.5; //Should give us back 50% of the building cost
         document.getElementById(id).innerHTML = building.level;
         document.getElementById(costID).innerHTML = building.cost;
+
+        //Updates the global cps counter 
+        globalCPS -= building.cps
+        
         
     }  
 }
