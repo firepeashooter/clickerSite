14ede48017a3c66aff6f667f675f6a263aeeaf1e
Ben Braun <benjamin.braun2003@gmail.com>
2025-04-25 16:58:13 -0400

    Cookie bobbing functionality on click

--- scripts/cookieUp.js
+++ scripts/cookieUp.js
@@ -4,11 +4,21 @@ let clickMultiplier = 1;
 let clickUpgrade = true
 
 function cookieUp(clickMultiplier){
+
+    cookie_image = document.getElementById('cookie');
+
+    //Interactive Cookie!
+    cookie_image.style.transform = "scale(1.1)";
+    setTimeout(() => {cookie_image.style.transform = 'scale(1)';}, 1)
     
+    //Increases the cookie counter from clicking
     cookies += (1 * clickMultiplier);
     document.getElementById('counter').innerHTML = cookies;
     
 }
+
+
+
 
 //When we buy the upgrade, we want it to go away or get greyed out, and then permanently increase our clickMultiplier
 
@@ -19,6 +29,7 @@ function upgradeClick(){
 
     if (clickUpgrade){
         clickMultiplier *= 2;
+        //In this case we just switch the image to signify that it's been created
         document.getElementById("upgrade1").src = "images/cancel.jpg";
         clickUpgrade = false;
     }
