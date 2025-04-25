b35d4a0fdf5a53c0a0642bebd1a054c88b8b4cdb
Ben Braun <116527926+firepeashooter@users.noreply.github.com>
2025-04-25 17:26:20 -0400

    Make the brett cookie interactive
    * Cookie bobbing functionality on click
    
    * Interactive Cookie on Hover
    
    * Enhanced the cookie hover

--- scripts/cookieUp.js
+++ scripts/cookieUp.js
@@ -4,11 +4,37 @@ let clickMultiplier = 1;
 let clickUpgrade = true
 
 function cookieUp(clickMultiplier){
+
+    cookie_image = document.getElementById('cookie');
+
+    //Interactive Cookie!
+    cookie_image.style.transform = "scale(1.1)";
+    setTimeout(cookieNormal, 10)
     
+    //Increases the cookie counter from clicking
     cookies += (1 * clickMultiplier);
     document.getElementById('counter').innerHTML = cookies;
     
 }
+
+
+//Function for scaling up the cookie
+function cookieBig(){
+    cookie_image = document.getElementById('cookie');
+
+    cookie_image.style.transform = "scale(1.1)";
+    setTimeout(cookieNormal, 100)
+}
+
+//Function for making the cookie the original size again
+function cookieNormal(){
+    cookie_image = document.getElementById('cookie');
+
+    cookie_image.style.transform = "scale(1)";
+}
+
+
+
 
 //When we buy the upgrade, we want it to go away or get greyed out, and then permanently increase our clickMultiplier
 
@@ -19,6 +45,7 @@ function upgradeClick(){
 
     if (clickUpgrade){
         clickMultiplier *= 2;
+        //In this case we just switch the image to signify that it's been created
         document.getElementById("upgrade1").src = "images/cancel.jpg";
         clickUpgrade = false;
     }
