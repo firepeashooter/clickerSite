8afd18c6cead7dc0da7ac910bf61d2206ff77be1
Ben Braun <benjamin.braun2003@gmail.com>
2025-05-06 22:01:01 -0400

    Basic Template for UpgradeTooltip

--- scripts/upgradeTooltip.js
+++ scripts/upgradeTooltip.js
@@ -4,7 +4,7 @@ document.addEventListener('mousemove', fn, false);
 
 function fn(e) { //Runs everytime the mouse moves
     for (var i=tooltip.length; i--;) { //Loops through all the tooltips in tooltip and brings them all to the cursor
-        tooltip[i].style.left = e.pageX + 'px';
-        tooltip[i].style.top = e.pageY + 'px';
+        tooltip[i].style.left = (e.pageX - 10) + 'px';
+        tooltip[i].style.top = (e.pageY + 10) + 'px';
     }
 } //Only the hover tooltip is shown thanks to the CSS .upgrade:hover .upgrade--tooltip class spec
